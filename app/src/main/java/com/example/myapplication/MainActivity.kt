03b498167705293a31774da19ca2b3cd10cfce4a
vlaf27449 <33995288+vlaf27449@users.noreply.github.com>
2021-09-08 14:19:45 +0700

    Урок 14

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,31 +14,7 @@ class MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        bindingClass.btResult.setOnClickListener {
 
-            val name = bindingClass.edName.text.toString().toLowerCase()
-            val password = bindingClass.edPassword.text.toString().toInt()
-
-            bindingClass.tvResult.visibility = View.VISIBLE
-
-            if (name == Constanse.DIRECTOR_NAME && password == Constanse.DIRECTOR_PASSWORD) {
-                val tempResult = "Ваша зарплата ${Constanse.DIRECTOR_SALARY}"
-                bindingClass.tvResult.text = tempResult
-                bindingClass.imPhoto.setImageResource(R.drawable.egor)
-            } else if (name == Constanse.INGENIR_NAME && password == Constanse.INGENIR_PASSWORD){
-                val tempResult = "Ваша зарплата ${Constanse.INGENIR_SALARY}"
-                bindingClass.tvResult.text = tempResult
-                bindingClass.imPhoto.setImageResource(R.drawable.sergey)
-            } else if (name == Constanse.DVORNIK_NAME && password == Constanse.DVORNIK_PASSWORD){
-                val tempResult = "Ваша зарплата ${Constanse.DVORNIK_SALARY}"
-                bindingClass.tvResult.text = tempResult
-                bindingClass.imPhoto.setImageResource(R.drawable.ivan)
-            } else {
-                bindingClass.tvResult.text = "Неверный логин или пароль"
-                bindingClass.imPhoto.setImageResource(R.drawable.dula)
-            }
-
-        }
 
     }
 
